1417f447ef1102a297dfd4eb22a4ebf8c13c73a8
Partonetrain <Partonetrain@gmail.com>
2020-11-19 20:28:12 -0600

    Finish Phantasmal, Start Dragonsteel

--- src/main/java/info/partonetrain/iafgear/trait/PhantasmalTrait.java
+++ src/main/java/info/partonetrain/iafgear/trait/PhantasmalTrait.java
@@ -3,8 +3,9 @@ package info.partonetrain.iafgear.trait;
 import com.github.alexthe666.iceandfire.event.ServerEvents;
 import com.github.alexthe666.iceandfire.item.IafItemRegistry;
 import info.partonetrain.iafgear.IAFGear;
-import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResultType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.silentchaos512.gear.api.traits.ITraitSerializer;
 import net.silentchaos512.gear.gear.trait.SimpleTrait;
@@ -16,14 +17,10 @@ public class PhantasmalTrait extends SimpleTrait {
 
     public static final ITraitSerializer<PhantasmalTrait> SERIALIZER = new Serializer<>(SERIALIZER_ID, PhantasmalTrait::new);
 
-    //I&F uses onItemUseFinish (left click) but this will do for now
     @Override
-    public ActionResultType onItemUse(ItemUseContext context, int traitLevel) {
-        if(context.getPlayer() != null){
-            //just use the event that's already there
-            ServerEvents.onLeftClick(context.getPlayer(), IafItemRegistry.GHOST_SWORD.getDefaultInstance());
-        }
-        return ActionResultType.PASS;
+    public void onItemSwing(ItemStack stack, LivingEntity entity, int traitLevel) {
+        //just use the event that's already there
+        ServerEvents.onLeftClick((PlayerEntity)entity, IafItemRegistry.GHOST_SWORD.getDefaultInstance());
     }
 
 
